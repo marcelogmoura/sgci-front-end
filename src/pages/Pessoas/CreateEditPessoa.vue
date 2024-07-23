8f4243ebc10772a70b3d697a6780508f7736188d
Marcelo Moura <93623867+marcelogmoura@users.noreply.github.com>
2024-07-23 09:43:06 -0300

    script para cadastro e classe

---
+++ src/pages/Pessoas/CreateEditPessoa.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>
+      <q-form ref="formPessoa" @submit="cadastrar" class="bg">
+
+      </q-form>
+      Teste template
+    </p>
+  </div>
+
+</template>
+
+<script>
+
+import { ref } from 'vue'
+
+export default {
+  name: 'CreateEditPessoa',
+  setup () {
+    const pessoa = ref({
+      nome: null
+    })
+    return {
+      pessoa
+    }
+  },
+  methods: {
+    cadastrar () {
+      console.log('cadastrar')
+    }
+  }
+}
+</script>
