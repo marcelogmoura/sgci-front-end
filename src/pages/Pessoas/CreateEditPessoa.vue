1105445027328041202ce0829eb1fa7c6da52300
Marcelo Moura <93623867+marcelogmoura@users.noreply.github.com>
2024-07-24 12:18:18 -0300

    formulario de cadastro para pessoa criado

--- src/pages/Pessoas/CreateEditPessoa.vue
+++ src/pages/Pessoas/CreateEditPessoa.vue
@@ -1,11 +1,78 @@
 <template>
   <div>
-    <p>
       <q-form ref="formPessoa" @submit="cadastrar" class="bg">
+      <div class="bg"></div>
+      <div class="main-container">
+        <div class="q-mb-md">
+        <h4 class="title">Cadastrar Pessoa</h4>
+        <div class="divisor-inline"></div>
+      </div>
+      <div class="q-pa-md" style="margin-top: -30px;">
+        <h4 class="subTitulo">Dados básicos: </h4>
+        <div class="row q-col-gutter-lg q-mb-sm">
+          <div class="col-6">
+            <q-input :rules="[ val => !(val == null || val == '') || 'Campo obrigatório' ]" v-model="pessoa.nome" label="Nome" dense/>
+          </div>
+          <div class="col-3">
+            <q-input v-model="pessoa.documento" label="Documento" dense/>
+          </div>
+          <div class="col-3">
+            <q-input v-model="pessoa.profissao" label="Profissão" dense/>
+          </div>
+
+            <div class="col-md-6">
+              <q-field dense label="Tipo de Pessoa" lazy-rules borderless stack-label>
+                  <q-option-group v-model="pessoa.tipo" :options="optionsTipoPessoa" color="primary" inline/>
+              </q-field>
+            </div>
+        </div>
+        <div class="col-md-6">     <!-- testar dentro da div acima-->
+              <q-field dense label="Estado Civil" lazy-rules borderless stack-label>
+                  <q-option-group v-model="pessoa.optionsEstadoCivil" :options="optionsEstadoCivil" color="primary" inline/>
+              </q-field>
+            </div>
+
+        <!-- campos-->
+      </div>
+
+      <div class="q-pa-md" style="margin-top: -30px;">
+        <h4 class="subTitulo">Endereço: </h4>
+        <div class="row q-col-gutter-lg q-mb-sm">
+          <div class="col-2">
+            <q-input :rules="[ val => !(val == null || val == '') || 'Campo obrigatório' ]" v-model="pessoa.endereco.cep" label="Cep" dense/>
+          </div>
+          <div class="col-3">
+            <q-input v-model="pessoa.endereco.estado" label="Estado" dense/>
+          </div>
+          <div class="col-4">
+            <q-input v-model="pessoa.endereco.cidade" label="Cidade" dense/>
+          </div>
+          <div class="col-3">
+            <q-input v-model="pessoa.endereco.bairro" label="Bairro" dense/>
+          </div>
+          <div class="col-9">
+            <q-input v-model="pessoa.endereco.rua" label="Rua" dense/>
+          </div>
+          <div class="col-3">
+            <q-input v-model="pessoa.endereco.numero" label="Número" dense/>
+          </div>
+        </div>
+        <!-- campos-->
+      </div>
+
+      <div class="row">
+        <div class="col-12">
+         <div style="float: right">
+                <!-- botoes-->
+         <q-btn style="margin-right: 10px;" label="Voltar" no-caps class="btn-voltar"/>
+         <q-btn type="submit" label="Cadastrar" no-caps class="btn-cadastrar"/></div>
+        </div>
+
+      </div>
+      </div>
 
       </q-form>
-      Teste template
-    </p>
+
   </div>
 
 </template>
@@ -18,10 +85,46 @@ export default {
   name: 'CreateEditPessoa',
   setup () {
     const pessoa = ref({
-      nome: null
+      nome: null,
+      documento: null,
+      profissao: null,
+      tipo: null,
+      estadoCivil: null,
+      endereco: ref({
+        cep: null,
+        estado: null,
+        cidade: null,
+        rua: null,
+        bairro: null,
+        numero: null
+      })
     })
     return {
-      pessoa
+      pessoa,
+      optionsTipoPessoa: [
+        {
+          label: 'Pessoa Física',
+          value: 'PESSOA_FISICA'
+        },
+        {
+          label: 'Pessoa Jurídica',
+          value: 'PESSOA_JURIDICA'
+        }
+      ],
+      optionsEstadoCivil: [
+        {
+          label: 'Solteiro',
+          value: 'SOLTEIRO'
+        },
+        {
+          label: 'Casado',
+          value: 'CASADO'
+        },
+        {
+          label: 'Divorciado',
+          value: 'DIVORCIADO'
+        }
+      ]
     }
   },
   methods: {
